debbe3a4e52475aa6f266abc2e6063c5e2e52e45
Duy <44285957+vuduy6220@users.noreply.github.com>
2019-01-18 22:17:16 +0700

    up code

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+//Home
+
 var TIMEOUT = 7000;
 
 
@@ -28,5 +30,60 @@ function handleNext() {
       .attr('checked', true);
 
   }
+
+}
+
+
+//Register
+
+var activeregister 
+
+function checkRegister() {
+  var result = true;
+  document.getElementById('messagerfname').innerHTML = '';
+  document.getElementById('messagerlname').innerHTML = '';
+  document.getElementById('messageremail').innerHTML = '';
+  document.getElementById('messagerusername').innerHTML = '';
+  document.getElementById('messagerpassword').innerHTML = '';
+  document.getElementById('messagercpassword').innerHTML = '';
+  var checkc1 = document.getElementById('rfname');
+    if (checkc1.value.length < 1) {
+      document.getElementById('messagerfname').innerHTML = 'You have not entered information yet!';
+      result = false;
+    }
+  var checkc2 = document.getElementById('rlname');
+    if (checkc2.value.length < 1) {
+      document.getElementById('messagerlname').innerHTML = 'You have not entered information yet!';
+      result = false;
+    }
+  var checkc3 = document.getElementById('remail');
+    if (checkc3.value.length < 1) {
+      document.getElementById('messageremail').innerHTML = 'You have not entered information yet!';
+      result = false;
+    }
+  var checkc4 = document.getElementById('rusername');
+    if (checkc4.value.length < 1) {
+      document.getElementById('messagerusername').innerHTML = 'You have not entered information yet!';
+      result = false;
+    }
+  var checkc5 = document.getElementById('rpassword');
+    if (checkc5.value.length < 1) {
+      document.getElementById('messagerpassword').innerHTML = 'You have not entered information yet!';
+      result = false;
+    }
+  var checkc6 = document.getElementById('rcpassword');
+    if (checkc6.value.length < 1) {
+      document.getElementById('messagercpassword').innerHTML = 'You have not entered information yet!';
+      result = false;
+    }
+    if (result == false) {
+      return false;
+    }
+  var checkboxterms = document.getElementById('acceptrregulations');
+    if (!checkboxterms.checked) {
+      alter("you must accept La_Imperial's Terms, Data Policy and Cookie Policy to be able to register!");
+      return false;
+    }alter("Thank you for accompany La_Imperial!");
+    return true;
 
 }
