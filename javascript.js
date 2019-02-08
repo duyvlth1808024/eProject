2457a025ac8c22966a3e844926d9907494ab2ce1
Duy <44285957+vuduy6220@users.noreply.github.com>
2019-02-08 15:34:13 +0700

    up

--- javascript.js
+++ javascript.js
@@ -85,3 +85,48 @@ function checkRegister() {
 function goBack() {
   window.history.back();
 };
+
+
+// Store location
+
+function initMap() {
+      var location1 = {lat: 21.0281772, lng: 105.7828902};
+      var location2 = {lat: 21.0273551, lng: 105.826772};
+      var location3 = {lat: 21.0308, lng: 105.8257971};
+      var map = new google.maps.Map(
+          document.getElementById('map'), {
+                          zoom: 15, 
+                          center: location1
+                          });
+      var marker = new google.maps.Marker({
+                        position: location1, 
+                        map: map,
+                        title:"Store 1"
+                        });
+      $(document).ready(function(){
+          $('#ls1').click(function(){
+            $('#ls2').removeClass('active-sls');
+            $('#ls3').removeClass('active-sls');
+            $('#ls1').addClass('active-sls');
+            map.setCenter(location1);
+            marker.setPosition(location1);
+            marker.setTitle("Store 1");
+          })
+          $('#ls2').click(function(){
+            $('#ls1').removeClass('active-sls');
+            $('#ls3').removeClass('active-sls');
+            $('#ls2').addClass('active-sls');
+            map.setCenter(location2);
+            marker.setPosition(location2);
+            marker.setTitle("Store 2");
+          })
+          $('#ls3').click(function(){
+            $('#ls1').removeClass('active-sls');
+            $('#ls2').removeClass('active-sls');
+            $('#ls3').addClass('active-sls');
+            map.setCenter(location3);
+            marker.setPosition(location3);
+            marker.setTitle("Store 3");
+          })
+        });
+    }
