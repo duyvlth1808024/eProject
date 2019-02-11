63b0112396abed69a23703a825f1ee44a9f67fd6
duypham9669 <duypdth1808013@fpt.edu.vn>
2019-02-12 01:31:55 +0700

    up

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://duypham9669:69912110a@ds042688.mlab.com:42688/duypham9669' 
+};
---
+++ server.js
@@ -0,0 +1,213 @@
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
+
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var url = 'mongodb://duypham9669:69912110a@ds042688.mlab.com:42688/duypham9669';
+
+// var configDB = require('./config/database.js');
+// configuration ===============================================================
+mongoose.connect(url);
+// connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(express.static(__dirname + '/public'));
+
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// app.use(bodyParser.urlencoded({ extended:true}));
+// //parser application.json
+// app.use(bodyParser.json());
+// app.use(upload.array());
+// required for passport
+app.use(session({
+    secret: 'ilovescotchscotchyscotchscotch', // session secret
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+
+var bodyParser = require("body-parser");
+var multer = require("multer");
+
+var upload = multer();
+//parser application/x-www-from-urlencoded
+
+app.use(bodyParser.urlencoded({ extended:true}));
+//parser application.json
+app.use(bodyParser.json());
+app.use(upload.array());
+
+app.use(express.static('public'));
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://duypham9669:69912110a@ds042688.mlab.com:42688/duypham9669';
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connected........', url);
+  	// const db = client.db;
+    var collection = db.db();
+	var more_product = collection.collection("more_product");
+
+
+      app.get("/list_product",function(req,res){
+            more_product.find({}).toArray(function(err,result){
+                var json = {
+                    status: 0,
+                    message: "Fail",
+                    data: {}
+                };
+                if(err){
+                    console.log(err);
+                }else{
+                    json.status = 1;
+                    json.message = "Success";
+                    json.data = result;
+                }    
+                res.send(json);
+            });
+
+        });
+             //list quat
+          app.get("/list_product_quat",function(req,res){
+            more_product.find({pr_name:'quat'}).toArray(function(err,result){
+                var json = {
+                    status: 0,
+                    message: "Fail",
+                    data: {}
+                };
+                if(err){
+                    console.log(err);
+                }else{
+                    json.status = 1;
+                    json.message = "Success";
+                    json.data = result;
+                }    
+                res.send(json);
+            });
+        });
+
+     
+            app.post("/dang_ky", function(req,res){
+                var user = {
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                };
+                account.insert([user],function(err,result){
+                    if(err){
+                        res.send("fail");
+                    }else{
+                        res.send("dang ky thanh cong");
+                    }
+                });
+            });
+
+
+    app.post("/them_san_pham", function(req,res)
+    {
+        var product = {
+            pr_code: req.body.pr_code,
+            pr_name: req.body.pr_name,
+            pr_class: req.body.pr_class,
+            manufacturer: req.body.manufacturer,
+            pr_images1: req.body.pr_images1,
+            pr_images2: req.body.pr_images2,
+            pr_images3: req.body.pr_images3,
+            pr_images4: req.body.pr_images4,
+            pr_price: req.body.pr_price,
+            pr_detail: req.body.pr_detail,
+            pr_guarantee: req.body.pr_guarantee,
+        };
+        more_product.insert([product],function(req,result){
+            if (err){
+                res.send("Fail");
+            }else{
+                res.send("da them san pham")
+            }
+        });
+    });
+
+        app.get("/detail_product",function(req,res){
+        var id = req.query.id;
+       more_product.find({_id: mongodb.ObjectId(id)}).toArray(function (err, result) {
+      var json = {
+        status: 0,
+        message: "Fail",
+        data: {}
+      };
+        if(err){
+          //console.log(err);
+        }else if(result.length < 1){
+          json.message = 'product not found';
+        }else{
+          json.status = 1;
+          json.message = "Success";
+          json.data = result[0];
+        }
+        res.send(json);
+      });
+ 		 });   
+
+     
+      //sửa sản phẩm
+      app.post("/edit_product",function(req,res){
+            var product2 = {
+            pr_code: req.body.pr_code,
+            pr_name: req.body.pr_name,
+            pr_images1: req.body.pr_images1,
+            pr_images2: req.body.pr_images2,
+            pr_images3: req.body.pr_images3,
+            pr_images4: req.body.pr_images4,
+            pr_price: req.body.pr_price,
+            pr_detail: req.body.pr_detail,
+            pr_guarantee: req.body.pr_guarantee
+         };
+          more_product.update({_id:mongodb.ObjectId(req.body._id)}, {$set: product2}, 
+         function (err, result) {
+          if(err){
+        res.send("Fail");
+       }else{
+        res.redirect("/index_admin");
+        }
+         });
+        });
+     //xoa
+    
+
+  }
+});
